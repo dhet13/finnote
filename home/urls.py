4fbbf44041364fe094afc380ba64f770ac614e2e
imps <elecsboy2@gmail.com>
2025-09-16 09:48:04 +0900

    home: 매매일지 연동을 위한 초기 작업

--- home/urls.py
+++ home/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('post/<int:post_id>/hide/', views.hide_post, name='hide_post'),
     path('post/<int:post_id>/report/', views.report_post, name='report_post'),
+    path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('comment/<int:comment_id>/reply/', views.create_reply, name='create_reply'),
 ]
